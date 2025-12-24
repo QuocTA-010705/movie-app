6d7819ec61c933a2f64dde2185cf7350305125c2
QuocTA-010705 <quoctran.010705@gmail.com>
2025-12-24 19:31:42 +0700

    12.24.2025

--- pages/movie-detail/index.jsx
+++ pages/movie-detail/index.jsx
@@ -11,6 +11,7 @@ import { GoPlay } from "react-icons/go";
 function Movie_Detail() {
   const { movieId } = useParams();
   const [movie, setMovie] = useState({});
+  const [credit, setCredit] = useState({});
   console.log(movieId);
   //https://api.themoviedb.org/3/movie/1087192?api_key=a10ee5569194b352bcca20840b7f8a32
   const fetchMovie_detail = async () => {
@@ -20,24 +21,38 @@ function Movie_Detail() {
     console.log(response.data);
     setMovie(response.data);
   };
+  const fetchCredit = async () => {
+    const response = await axios.get(`
+      https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=a10ee5569194b352bcca20840b7f8a32`);
+    console.log(response.data);
+    setCredit(response.data);
+  };
   useEffect(() => {
     fetchMovie_detail();
+    fetchCredit();
   }, [movieId]);
-  const formatRuntime = (minutes) =>{
-    if(!minutes) return "N/A";
+  //filter trả về array còn find trả về chỉ 1 object nhưng để tránh crash thì thêm || [] ở cuối filter
+  const filterDirector = credit?.crew?.find((item) => item.job === "Director");
+  const filterWriter =
+    credit?.crew?.filter(
+      (item) => item.job === "Screenplay" || item.job === "Story"
+    )|| [];
+
+  const formatRuntime = (minutes) => {
+    if (!minutes) return "N/A";
     const hours = Math.floor(minutes / 60);
     const mins = minutes % 60;
     return `${hours}h ${mins}m`;
-  }
+  };
   const formatDate = (dateString) => {
-    if(!dateString) return "N/A";
+    if (!dateString) return "N/A";
     const date = new Date(dateString);
     return date.toLocaleDateString("en-US", {
       month: "short",
       day: "2-digit",
       year: "numeric",
     });
-  }
+  };
   return (
     <div className="container_wrapper">
       <Header />
@@ -87,21 +102,42 @@ function Movie_Detail() {
             </div>
             <div className="overview">
               <h1>Overview</h1>
-              <div className="description">
-                {movie?.overview}
-              </div>
+              <div className="description">{movie?.overview}</div>
             </div>
             <div className="description">
               <div className="description__status">
-                <h4>Status: <span>{movie?.status}</span></h4>
-                <h4>Release Date: <span>{formatDate(movie?.release_date)}</span></h4>
-                <h4>Runtime: <span>{formatRuntime(movie?.runtime)}</span></h4>
+                <h4>
+                  Status: <span>{movie?.status}</span>
+                </h4>
+                <h4>
+                  Release Date: <span>{formatDate(movie?.release_date)}</span>
+                </h4>
+                <h4>
+                  Runtime: <span>{formatRuntime(movie?.runtime)}</span>
+                </h4>
+              </div>
+              <div className="description__director">
+                <h4>
+                  Director: <span>{filterDirector?.name}</span>
+                </h4>
+              </div>
+              <div className="description__writer">
+                <h4>
+                  Writer:
+                  {filterWriter.slice(0, 6).map((item,index, arr) => (
+                    <span key={item.id}>{item?.name} {index < arr.length -1 && ", "}
+                    </span>
+                  ))}
+                </h4>
               </div>
             </div>
           </div>
         </div>
+        <div className="overlay"></div>
+        <div className="top_cast">
+        <h1>Top Cast</h1>
       </div>
-      <div className="overlay"></div>
+      </div>
     </div>
   );
 }
